2cfd532b7e5db59f06c0d1ba4ee053e436ea612d
Jasonjys <jasonjys0511@gmail.com>
2018-10-05 22:05:38 -0400

    tests for isStraightFlush cases

--- src/test/java/TestStraightFlush.java
+++ src/test/java/TestStraightFlush.java
@@ -1,7 +1,10 @@
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestStraightFlush {
     private final PokerGame game = new PokerGame();
