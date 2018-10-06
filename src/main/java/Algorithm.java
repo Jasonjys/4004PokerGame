5fdaef77c244c5c54fc3389d7f8e9d94e3ab4518
Jasonjys <jasonjys0511@gmail.com>
2018-10-05 22:30:52 -0400

    logic for isFlush

--- src/main/java/Algorithm.java
+++ src/main/java/Algorithm.java
@@ -3,6 +3,14 @@ import java.util.*;
 public class Algorithm {
     public Algorithm() {}
 
+    private static HashSet<String> buildSuitSet (List<Card> hand) {
+        HashSet<String> set = new HashSet<String>();
+        for(Card card:hand) {
+            set.add(card.getSuit());
+        }
+        return set;
+    }
+
     public static boolean isTenToFourteen (List<Card> hand) {
         return hand.get(0).getRank() == 1
                 && hand.get(1).getRank() == 10
@@ -11,5 +19,16 @@ public class Algorithm {
                 && hand.get(4).getRank() == 13;
     }
 
+    public static Result isFlush (List<Card> hand) {
+        HashSet<String> set = buildSuitSet(hand);
+        Collections.sort(hand, new CardSorter());
 
+        if (set.size() == 1) {
+            if (isTenToFourteen(hand) || hand.get(0).getRank() == 1) {
+                return new Result(true, hand.get(0), null);
+            }
+            return new Result(true, hand.get(4), null);
+        }
+        return new Result(false, null, null);
+    }
 }
