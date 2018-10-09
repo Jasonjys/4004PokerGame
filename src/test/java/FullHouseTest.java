3b431d134a1a46e81f719ec3b65e2971ec490c4b
Jasonjys <jasonjys0511@gmail.com>
2018-10-08 23:05:04 -0400

    add tests for is full house

---
+++ src/test/java/FullHouseTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class FullHouseTest {
+    private final PokerGame game = new PokerGame();
+
+    @ParameterizedTest
+    @ValueSource(strings = {"D8 D4 C8 S8 S4", "S4 SA D4 DA C4", "S7 D7 HK C7 DK", "CA D5 H5 DA SA"})
+    void fullHouseWithRandomOrderShouldPass(String fullHouse) {
+        game.setPlayerHand(0, fullHouse);
+        Player aip = game.getPlayer(0);
+
+        Assertions.assertTrue(Algorithm.isFullHouse(aip.getHand()).isMatched());
+    }
+
+    @Test
+    void nonFlushShouldFail () {
+        game.setPlayerHand(0, "S2 C9 H9 DJ SA");
+        Player aip = game.getPlayer(0);
+
+        Assertions.assertFalse(Algorithm.isFullHouse(aip.getHand()).isMatched());
+    }
+}
